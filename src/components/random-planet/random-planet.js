7aec5ad1ba076e5c7e92c8bd8169a8394a230172
Ekaterina Ustinova <galateya2406@yandex.ru>
2024-03-11 15:19:32 +0400

    Изменения 1103

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -13,9 +13,10 @@ export default class RandomPlanet extends Component {
     loading: true
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 5000);
+    // clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
@@ -33,9 +34,8 @@ export default class RandomPlanet extends Component {
     })
   };
 
-  updatePlanet() {
-    const id = Math.floor(Math.random()*25) + 2;
-    // const id = 12000;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random()*25) + 3;
     this.swapiService.getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
